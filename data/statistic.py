8265032a3a46f3f6157881dd1e8a707d35952383
Perlovskyi Volodymyr <volodymyr.perlovskiy@gmail.com>
2024-06-13 19:52:20 +0300

    Make single healthckeck and format message with marks

--- data/statistic.py
+++ data/statistic.py
@@ -1,23 +1,10 @@
-import prettytable as pt
-from telegram import ParseMode
-from telegram import CallbackContext, Updater
+from health_check import drift
 
+def set_marks():
+    content = tuple(drift.current_status())
+    if len(content) != 0 and content[1] == True:
+        marker="✅"
+    if len(content) != 0 and content[1] == False:
+        marker="❌"
 
-def send_table(update: Updater, context: CallbackContext):
-    table = pt.PrettyTable(['Symbol', 'Price', 'Change'])
-    table.align['Symbol'] = 'l'
-    table.align['Price'] = 'r'
-    table.align['Change'] = 'r'
-
-    data = [
-        ('ABC', 20.85, 1.626),
-        ('DEF', 78.95, 0.099),
-        ('GHI', 23.45, 0.192),
-        ('JKL', 98.85, 0.292),
-    ]
-    for symbol, price, change in data:
-        table.add_row([symbol, f'{price:.2f}', f'{change:.3f}'])
-
-    
-    # or use markdown
-    update.message.reply_text(f'```{table}```', parse_mode=ParseMode.MARKDOWN_V2)
+    return marker
--- handlers/questions.py
+++ handlers/questions.py
@@ -2,6 +2,13 @@ from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardRemove
 from keyboards.for_questions import make_choice_kb
+from health_check import drift
+from data import statistic
+from cache import scan_cache
+from aiogram.enums import ParseMode
+from aiogram.utils.formatting import (
+    Bold, as_list, as_marked_section, as_key_value, HashTag
+)
 # from data import statistic
 
 router = Router()  # [1]
@@ -15,14 +22,25 @@ async def cmd_start(message: Message):
 
 @router.message(F.text.lower() == "перевірити наявність єлектрики в лбц")
 async def answer_check_electrisity(message: Message):
+    current_datetime = tuple(drift.current_status())
+    appropriate_mark = statistic.set_marks()
     await message.answer(
-        "Перевірка",
+        f"{current_datetime[0]} {appropriate_mark}",       
         reply_markup=make_choice_kb()
     )
 
 @router.message(F.text.lower() == "отримати статистику за тиждень")
 async def answer_get_statistic(message: Message):
+    ls = scan_cache.arrange_dict(scan_cache.dict_cache)
     await message.answer(
-        "Тут має бути листінг",
-        reply_markup=ReplyKeyboardRemove()
+        f"<b>{ls}</b>",
+        parse_mode=ParseMode.HTML,
+        reply_markup=make_choice_kb()       
     )
+
+# @router.message(F.text.lower() == "отримати статистику за тиждень")
+# async def answer_get_statistic(message: Message):
+#    await message.answer(
+#        "Тут має бути листінг",
+#        reply_markup=ReplyKeyboardRemove()
+#    )
