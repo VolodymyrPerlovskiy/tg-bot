9b54684191fb116e81130865d14f1459e69d73ca
Perlovskyi Volodymyr <volodymyr.perlovskiy@gmail.com>
2024-07-25 13:38:47 +0300

    date time format change

--- health_check/drift.py
+++ health_check/drift.py
@@ -5,7 +5,7 @@ from redis_dict import RedisDict
 Host = ""
 next_call = time.time()
 
-dict = RedisDict(host="127.0.0.1", port="6379", password="", db=0, expire=timedelta(minutes=10080))
+dict = RedisDict(host="127.0.0.1", port="6379", password="", db=0, expire=timedelta(minutes=1440))
 
 
 def ping_ok(sHost) -> bool:
@@ -19,14 +19,14 @@ def ping_ok(sHost) -> bool:
     return True
 
 def current_status():
-    aware_local_now = datetime.now(timezone.utc).astimezone().strftime("%m/%d/%Y-%H:%M:%S")
+    aware_local_now = datetime.now(timezone.utc).astimezone().strftime("%m/%d/%Y-%H:%M")
     current_state = ping_ok(Host)
     return aware_local_now, current_state
 
 # Write health check result to redis constantly
 def write_2_cache():
     global next_call
-    aware_local_now = datetime.now(timezone.utc).astimezone().strftime("%m/%d/%Y-%H:%M:%S")
+    aware_local_now = datetime.now(timezone.utc).astimezone().strftime("%m/%d/%Y-%H:%M")
     current_state = ping_ok(Host) 
     next_call = next_call+3600
     dict[str(aware_local_now)] = str(current_state) #, ex=604800
